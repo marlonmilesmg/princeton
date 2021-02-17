30068ae00723af428e98b99487c6b40ccae2a079
Marlon Magonjo <39700735+marlonmilesmg@users.noreply.github.com>
2021-02-17 10:49:38 +0200

    fix input capture format

--- HelloGoodbye.java
+++ HelloGoodbye.java
@@ -3,12 +3,7 @@
 public class HelloGoodbye {
     public static void main(String[] args) {
 
-        String name, name1;
-
-        name = StdIn.readString();
-        name1 = StdIn.readString();
-
-        System.out.println("Hello " + name + " and " + name1);
-        System.out.println("Goodbye " + name1 + " and " + name);
+        System.out.println("Hello " + args[0] + " and " + args[1]);
+        System.out.println("Goodbye " + args[1] + " and " + args[0]);
     }
 }
