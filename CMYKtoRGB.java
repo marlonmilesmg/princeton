bb7f85dfd67ceba4603c87a85797049f531dbb2c
Marlon Magonjo <39700735+marlonmilesmg@users.noreply.github.com>
2021-02-17 09:54:23 +0200

    fix input capture format

--- CMYKtoRGB.java
+++ CMYKtoRGB.java
@@ -2,21 +2,22 @@ public class CMYKtoRGB {
     public static void main(String[] args) {
 
         double white;
-        int r, g, b;
+        int r, g, blue;
 
         double c = Double.parseDouble(args[0]);
         double m = Double.parseDouble(args[1]);
         double y = Double.parseDouble(args[2]);
         double k = Double.parseDouble(args[3]);
 
-        b = (int) (255 * (1 - y) * (1 - k));
-        white = 1 - b;
-        r = (int) ((int) (255 * white) * (1 - c));
-        g = (int) ((int) (255 * white) * (1 - m));
+
+        white = 1 - k;
+        r = (int) Math.round(255 * white * (1 - c));
+        g = (int) Math.round(255 * white * (1 - m));
+        blue = (int) Math.round(255 * white *(1 - y));
 
         System.out.println("Red = " + r);
         System.out.println("Green = " + g);
-        System.out.println("Blue = " + b);
+        System.out.println("Blue = " + blue);
 
     }
 }
--- RightTriangle.java
+++ RightTriangle.java
@@ -1,28 +1,18 @@
 public class RightTriangle {
     public static void main(String[] args) {
 
-        int a2, b2, c2, m1, m2, m3;
+        int a2, b2;
+        boolean isRightTriangle;
 
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         int c = Integer.parseInt(args[2]);
 
-        a2 = (int) Math.pow(a, 2);
-        b2 = (int) Math.pow(b, 2);
-        c2 = (int) Math.pow(c, 2);
+        a2 = (a * a) + (b * b);
+        b2 = (c * c);
 
-        m1 = a2 + b2;
-        m2 = a2 + c2;
-        m3 = b2 + c2;
+        isRightTriangle = (a2 == b2);
+        System.out.println(isRightTriangle);
 
-        if (a > 0 && b > 0 && c > 0) {
-            if (m1 == c2 || m3 == a2 || m2 == b2) {
-                System.out.println("true");
-            } else {
-                System.out.println("false");
-            }
-        } else {
-            System.out.println("false");
-        }
     }
 }
