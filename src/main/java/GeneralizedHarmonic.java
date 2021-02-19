069d60e8f0f3319d7a287cdb436aff58197c7749
Marlon Magonjo <39700735+marlonmilesmg@users.noreply.github.com>
2021-02-19 14:12:59 +0200

    harmonic

--- src/main/java/GeneralizedHarmonic.java
+++ src/main/java/GeneralizedHarmonic.java
@@ -3,6 +3,11 @@ public class GeneralizedHarmonic {
     int n = Integer.parseInt(args[0]);
     int r = Integer.parseInt(args[1]);
 
+        double sum = 0.0;
+        for (r = 1; r <= n; r++) {
+            sum += 1.0 / r;
+        }
 
+        System.out.println(sum);
     }
 }
