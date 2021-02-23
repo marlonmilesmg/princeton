a6ca6ce90c48ed397806134be4c5fc54324f4a99
Marlon Magonjo <39700735+marlonmilesmg@users.noreply.github.com>
2021-02-23 08:48:03 +0200

    RandomWalker and RandomWalkers

--- src/main/java/RandomWalkers.java
+++ src/main/java/RandomWalkers.java
@@ -1,6 +1,41 @@
 public class RandomWalkers {
-    public static void main(String[] args) {
+    public static void main(String [] args){
+
         int r = Integer.parseInt(args[0]);
-        int trials = Integer.parseInt(args[1]);
+        int trials =  Integer.parseInt(args[1]);
+        int totalSteps = 0;
+        double averageSteps = 0;
+
+        for(int i = 1 ; i <= trials ; i++){
+            int x = 0;
+            int y = 0;
+            int distance = 0;
+            int steps = 0;
+
+            while(distance != r){
+                double random = Math.random();
+                if(random > 0 && random < 0.25){
+                    x = x + 1;
+                    steps++;
+                }
+                if(random > 0.25 && random < 0.50){
+                    x = x - 1;
+                    steps++;
+                }
+                if(random > 0.50 && random < 0.75){
+                    y = y + 1;
+                    steps++;
+                }
+                if(random > 0.75 && random < 1){
+                    y = y - 1;
+                    steps++;
+
+                }
+                distance = Math.abs(x) + Math.abs(y);
+            }
+            totalSteps = totalSteps + steps;
+        }
+        averageSteps = ((double)totalSteps / trials);
+        System.out.println("average steps " + averageSteps);
     }
 }
--- src/main/java/RandonWalker.java
+++ src/main/java/RandonWalker.java
@@ -1,5 +1,23 @@
-public class RandonWalker {
-    public static void main(String[] args) {
+public class RandomWalker {
 
+    public static void main(String args[]) {
+
+        int r = Integer.parseInt(args[0]);
+        int x = 0;
+        int y = 0;
+        int step = 0;
+        double R;
+
+        while(Math.abs(x) + Math.abs(y) < r ){
+            step++;
+            R = Math.random();
+            if(R <= 0.25) x++;
+            else if (R <= 0.50) x--;
+            else if (R <= 0.75) y++;
+            else if (R <= 1.00) y--;
+
+            System.out.println("(" + x + ", " + y + ")" );
+        }
+        System.out.println("steps = " + step);
     }
 }
